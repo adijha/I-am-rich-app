3dcf353e07aa48e878ff6f0930884133df5a2014
Aditya Kumar Jha <adityajha126@gmail.com>
2019-08-26 02:29:49 +0530

    challange 1

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('I am rich'),
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
       ),
     ),
   );
